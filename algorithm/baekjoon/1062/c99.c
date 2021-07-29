527b6860dd332105f699fc981ca25b5d950c263c
Kal-MH <rkfalgus713@gmail.com>
2021-07-29 20:32:48 +0900

    Update c99.c
    set bounds of backtracking(25 -> 26)

--- algorithm/baekjoon/1062/c99.c
+++ algorithm/baekjoon/1062/c99.c
@@ -10,7 +10,7 @@ int	used[26];
 int	check(int i)
 {
 	return (i == 0 || i == ('n' - 'a') || i == ('t' - 'a') || i == ('c' - 'a')
-			|| i == ('i' - 'a'));
+		|| i == ('i' - 'a'));
 }
 
 void	backtrack(int i, int n)
@@ -20,19 +20,19 @@ void	backtrack(int i, int n)
 	if (n == 0)
 	{
 		cnt = 0;
-		for(int j = 0; j < N; j++)
+		for (int j = 0; j < N; j++)
 		{
 			know = 1;
-			for(int k = 0; k < 25; k++)
+			for (int k = 0; k < 26; k++)
 				if (word[j][k] && !used[k])
 					know = 0;
 			if (know)
 				cnt++;
 		}
-		ans = (ans > cnt) ? ans : cnt;
-		return ;
+		ans = (ans >= cnt) ? ans : cnt;
+		return;
 	}
-	for(int j = i; j < 25; j++)
+	for (int j = i; j < 26; j++)
 	{
 		if (check(j))
 			continue;
@@ -48,7 +48,7 @@ int	main()
 	char	str[16];
 
 	scanf("%d %d", &N, &K);
-	for(i = 0;i < N; i++)
+	for (i = 0; i < N; i++)
 	{
 		scanf("%s", str);
 		j = -1;
