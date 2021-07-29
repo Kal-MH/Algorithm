3063f1bebe804f09b6dc6b6eb39fd5bf836fec73
Kal-MH <rkfalgus713@gmail.com>
2021-07-29 20:20:05 +0900

    Update c99.c
    fix bug with MOD option
    - remove it (tmp1 < 0)
    add printf("\n");

--- algorithm/baekjoon/3425/c99.c
+++ algorithm/baekjoon/3425/c99.c
@@ -32,7 +32,7 @@ void	init_stack()
 void	push(int num)
 {
 	if (top + 1 == MAX_S)
-		return ;
+		return;
 	stack[++top] = num;
 }
 
@@ -57,215 +57,211 @@ int	main()
 		while (1)
 		{
 			scanf("%s", in);
-			switch(in[0])
+			switch (in[0])
 			{
-				case 'N' :
-					scanf("%d", &command[i][1]);
-					command[i++][0] = NUM;
-					break ;
-				case 'P' :
-					command[i++][0] = POP;
-					break ;
-				case 'I' : 
-					command[i++][0] = INV;
-					break ;
-				case 'D' :
-					if (in[1] == 'U')
-						command[i++][0] = DUP;
-					else
-						command[i++][0] = DIV;
-					break ;
-				case 'S' :
-					if (in[1] == 'W')
-						command[i++][0] = SWP;
-					else
-						command[i++][0] = SUB;
-					break ;
-				case 'A' :
-					command[i++][0] = ADD;
-					break ;
-				case 'M' : 
-					if (in[1] == 'O')
-						command[i++][0] = MOD;
-					else
-						command[i++][0] = MUL;
-					break ;
-				case 'E' : 
-					end = 1; break ;
-				case 'Q' : 
-					quit = 1; break ;
+			case 'N':
+				scanf("%d", &command[i][1]);
+				command[i++][0] = NUM;
+				break;
+			case 'P':
+				command[i++][0] = POP;
+				break;
+			case 'I':
+				command[i++][0] = INV;
+				break;
+			case 'D':
+				if (in[1] == 'U')
+					command[i++][0] = DUP;
+				else
+					command[i++][0] = DIV;
+				break;
+			case 'S':
+				if (in[1] == 'W')
+					command[i++][0] = SWP;
+				else
+					command[i++][0] = SUB;
+				break;
+			case 'A':
+				command[i++][0] = ADD;
+				break;
+			case 'M':
+				if (in[1] == 'O')
+					command[i++][0] = MOD;
+				else
+					command[i++][0] = MUL;
+				break;
+			case 'E':
+				end = 1; break;
+			case 'Q':
+				quit = 1; break;
 			}
-			if (end || quit) break ;
+			if (end || quit) break;
 		}
 		if (quit)
-			break ;
+			break;
 
 		//input value
 		scanf("%d", &n);
-		for(j = 0; j < n; j++)
+		for (j = 0; j < n; j++)
 			scanf("%d", program + j);
 		//run
-		for(j = 0;j < n; j++)
+		for (j = 0; j < n; j++)
 		{
 			init_stack();
 			push(program[j]);
 			error = 0;
-			for(k = 0; k < i; k++)
+			for (k = 0; k < i; k++)
 			{
 				abs = 0;
-				switch(command[k][0])
+				switch (command[k][0])
 				{
-					case NUM :
-						push(command[k][1]);
-						break ;
-					case POP :
-						if (top >= 0)
-							pop();
-						else
+				case NUM:
+					push(command[k][1]);
+					break;
+				case POP:
+					if (top >= 0)
+						pop();
+					else
+						error = 1;
+					break;
+				case INV:
+					if (top >= 0)
+					{
+						tmp1 = pop();
+						push(-tmp1);
+					}
+					else
+						error = 1;
+					break;
+				case DUP:
+					if (top >= 0)
+					{
+						tmp1 = pop();
+						push(tmp1);
+						push(tmp1);
+					}
+					else
+						error = 1;
+					break;
+				case SWP:
+					if (top >= 1)
+					{
+						tmp1 = pop();
+						tmp2 = pop();
+						push(tmp1);
+						push(tmp2);
+					}
+					else
+						error = 1;
+					break;
+				case ADD:
+					if (top >= 1)
+					{
+						tmp1 = pop();
+						tmp2 = pop();
+						res = tmp2 + tmp1;
+						if (res > ABS_MAX || res < ABS_MIN)
 							error = 1;
-						break ;
-					case INV : 
-						if (top >= 0)
+						else
+							push(res);
+					}
+					else
+						error = 1;
+					break;
+				case SUB:
+					if (top >= 1)
+					{
+						tmp1 = pop();
+						tmp2 = pop();
+						res = tmp2 - tmp1;
+						if (res > ABS_MAX || res < ABS_MIN)
+							error = 1;
+						else
+							push(res);
+					}
+					else
+						error = 1;
+					break;
+				case MUL:
+					if (top >= 1)
+					{
+						tmp1 = pop();
+						tmp2 = pop();
+						res = (double)tmp2 * tmp1;
+						if (res > ABS_MAX || res < ABS_MIN)
+							error = 1;
+						else
+							push(res);
+					}
+					else
+						error = 1;
+					break;
+				case DIV:
+					if (top >= 1)
+					{
+						tmp1 = pop();
+						tmp2 = pop();
+						if (tmp1 == 0)
 						{
-							tmp1 = pop();
-							push(-tmp1);
-						}
-						else
 							error = 1;
-						break ;
-					case DUP : 
-						if (top >= 0)
+							break;
+						}
+						if (tmp1 < 0)
 						{
-							tmp1 = pop();
-							push(tmp1);
-							push(tmp1);
+							tmp1 = -tmp1;
+							abs++;
 						}
-						else
-							error = 1;
-						break ;
-					case SWP : 
-						if (top >= 1)
+						else if (tmp2 < 0)
 						{
-							tmp1 = pop();
-							tmp2 = pop();
-							push(tmp1);
-							push(tmp2);
+							tmp2 = -tmp2;
+							abs++;
 						}
-						else
+						res = tmp2 / tmp1;
+						if (abs == 1)
+							res = -res;
+						if (res > ABS_MAX || res < ABS_MIN)
 							error = 1;
-						break ;
-					case ADD :
-						if (top >= 1)
+						else
+							push(res);
+					}
+					else
+						error = 1;
+					break;
+				case MOD:
+					if (top >= 1)
+					{
+						tmp1 = pop();
+						tmp2 = pop();
+						if (tmp1 == 0)
 						{
-							tmp1 = pop();
-							tmp2 = pop();
-							res = tmp2 + tmp1;
-							if (res > ABS_MAX || res < ABS_MIN)
-								error = 1;
-							else
-								push(res);
-						}
-						else
 							error = 1;
-						break ;
-					case SUB : 
-						if (top >= 1)
+							break;
+						}
+						if (tmp2 < 0)
 						{
-							tmp1 = pop();
-							tmp2 = pop();
-							res = tmp2 - tmp1;
-							if (res > ABS_MAX || res < ABS_MIN)
-								error = 1;
-							else
-								push(res);
+							tmp2 = -tmp2;
+							abs++;
 						}
-						else
+						res = tmp2 % tmp1;
+						if (abs == 1)
+							res = -res;
+						if (res > ABS_MAX || res < ABS_MIN)
 							error = 1;
-						break ;
-					case MUL : 
-						if (top >= 1)
-						{
-							tmp1 = pop();
-							tmp2 = pop();
-							res = (double)tmp2 * tmp1;
-							if (res > ABS_MAX || res < ABS_MIN)
-								error = 1;
-							else
-								push(res);
-						}
 						else
-							error = 1;
-						break ;
-					case DIV : 
-						if (top >= 1)
-						{
-							tmp1 = pop();
-							tmp2 = pop();
-							if (tmp1 == 0)
-							{
-								error = 1;
-								break ;
-							}
-							if (tmp1 < 0)
-							{
-								tmp1 = -tmp1;
-								abs++;
-							}
-							else if (tmp2 < 0)
-							{
-								tmp2 = -tmp2;
-								abs++;
-							}
-							res = tmp2 / tmp1;
-							if (abs == 1)
-								res = -res;
-							if (res > ABS_MAX || res < ABS_MIN)
-								error = 1;
-							else
-								push(res);
-						}
-						else
-							error = 1;
-						break ;
-					case MOD :
-						if (top >= 1)
-						{
-							tmp1 = pop();
-							tmp2 = pop();
-							if (tmp1 == 0)
-							{
-								error = 1;
-								break ;
-							}
-							if (tmp1 < 0)
-							{
-								tmp1 = -tmp1;
-								abs++;
-							}
-							else if (tmp2 < 0)
-							{
-								tmp2 = -tmp2;
-								abs++;
-							}
-							res = tmp2 % tmp1;
-							if (abs == 1)
-								res = -res;
-							if (res > ABS_MAX || res < ABS_MIN)
-								error = 1;
-							else
-								push(res);
-						}
-						else
-							error = 1;
-						break ;
+							push(res);
+					}
+					else
+						error = 1;
+					break;
 				}
 				if (error)
-					break ;
+					break;
 			}
 			if (error || top != 0)
 				printf("ERROR\n");
 			else
 				printf("%d\n", pop());
 		}
+		printf("\n");
 	}
 }
